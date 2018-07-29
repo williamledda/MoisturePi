c4ae3886641ffd5c41ec9dbab77203f252c3c21a
William Ledda <32984328+williamledda@users.noreply.github.com>
2018-07-29 15:18:08 +0200

    Systemd service
    - Added control from systemd
    - Made server less verbose
    - Refactoring of moisture_client

--- python/moisture_client.py
+++ python/moisture_client.py
@@ -4,26 +4,28 @@ Created on 17 set 2017
 @author: willy
 '''
 import socket
-import time
 import sys
 
-#UDP_IP = "127.0.0.1"
-#UDP_PORT = 6000
-#MESSAGE = "RAW?"
+if __name__ == '__main__':
+    UDP_IP = sys.argv[1]
+    UDP_PORT = int(sys.argv[2])
+    MESSAGE = sys.argv[3]
 
-UDP_IP = sys.argv[1]
-UDP_PORT = int(sys.argv[2])
-MESSAGE = sys.argv[3]
+    #print("UDP target IP:", UDP_IP)
+    #print("UDP target port:", UDP_PORT)
+    #print("message:", MESSAGE)
 
-#print("UDP target IP:", UDP_IP)
-#print("UDP target port:", UDP_PORT)
-#print("message:", MESSAGE)
+    sock = socket.socket(socket.AF_INET, # Internet 
+                         socket.SOCK_DGRAM) # UDP
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+    sock.settimeout(5)
 
-sock.settimeout(5)
-
-sock.sendto(bytes(MESSAGE, "UTF8"), (UDP_IP, UDP_PORT))
-(payload, remote) = sock.recvfrom(1024)
-print("{:.2f}".format(float(payload)))
+    sock.sendto(bytes(MESSAGE, "UTF8"), (UDP_IP, UDP_PORT))
+    (payload, remote) = sock.recvfrom(1024)
+    
+    if(MESSAGE == "RAW?" or MESSAGE == "PERC?"):
+        print("{:.2f}".format(float(payload)))
+    else:
+        print("Response: ", str(payload, "UTF8"))
+    exit()
+    
